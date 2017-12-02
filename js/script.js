0f9fab5fcc50d064bdac668bc3460966835a5edb
Kate Pozdnysheva <ekaterina.pozdnysheva@gmail.com>
2017-12-02 19:11:03 +0200

    refactored services menu toggle script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// @ts-check
 var write_us = document.querySelector(".button_link_wtite-us");
 var popup_form = document.querySelector(".popup_form");
 var closeFormButton = popup_form.querySelector(".popup__close");
@@ -7,8 +6,6 @@ var popup_map = document.querySelector(".popup_map");
 var closeMapButton = popup_map.querySelector(".popup__close");
 var form = popup_form.querySelector("form");
 var formSubmitButton = form.querySelector(".button[type='submit']");
-
-
 var user_name = document.getElementById("name");
 var user_email = document.getElementById("email");
 var user_message = document.getElementById("text-message");
@@ -48,27 +45,18 @@ window.addEventListener("keydown", function (evt) {
   popup_form.classList.remove("popup-open");
 });
 
+var servicesList = document.querySelector('.services__list');
+var currentService;
 
-const ul = document.querySelector('.services__list');
-ul.addEventListener('click', (e) => {
-  let current = document.querySelector('.services__service_current');
-  let target = e.target
-  while (target.tagName !== 'UL') {
-    console.log('tagName', target.tagName);
-    if (target.tagName === 'LI') {
-      current.classList.remove('services__service_current');
-      target.classList.add('services__service_current');
-    }
-    target = target.parentNode;
-  }
+servicesList.addEventListener('click', function (e) {
+  var button = e.target;
+  if (button.tagName !== 'BUTTON') return;
+
+  var currentServiceClassName = 'services__service_current';
+  if (!currentService) currentService = servicesList.querySelector(`.${currentServiceClassName}`);
+
+  var menuItem = button.closest('.services__service');
+  menuItem.classList.add(currentServiceClassName);
+  currentService.classList.remove(currentServiceClassName);
+  currentService = menuItem;
 });
-
-
-
-
-
-
-
-
-
-
